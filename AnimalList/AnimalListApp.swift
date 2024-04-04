deff218cd6dc02b6b962411b9157e018df18b672
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-05 01:57:03 +0200

    Initial Commit

---
+++ AnimalList/AnimalListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalListApp.swift
+//  AnimalList
+//
+//  Created by  Ixart on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimalListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
